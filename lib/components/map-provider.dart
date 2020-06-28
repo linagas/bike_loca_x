a7003f4d72b4f5a2b6b078bb72577a47356a896d
Deivy Hernandez <deivyhernandezperez@gmail.com>
2020-06-28 03:51:13 -0400

    create geolocation

--- lib/components/map-provider.dart
+++ lib/components/map-provider.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapProvider extends StatefulWidget {
+  LatLng _current = const LatLng(45.521563, -122.677433);
+
   @override
-  _MapProviderState createState() => _MapProviderState();
+  _MapProviderState createState() => _MapProviderState(_current);
+
+  Future<void> _getCurrentLocation() {
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      return LatLng(position.latitude, position.longitude);
+    }).catchError((e) {
+      print(e);
+    });
+  }
 }
 
 class _MapProviderState extends State<MapProvider> {
   GoogleMapController mapController;
+  LatLng _currentPosition;
 
-  final LatLng _center = const LatLng(-33.4539101, -70.6335762);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  _MapProviderState(LatLng _currentPosition) {
+    this._currentPosition = _currentPosition;
   }
 
   @override
@@ -21,16 +35,25 @@ class _MapProviderState extends State<MapProvider> {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Mapa BikeLoca'),
-          backgroundColor: Colors.green[700],
+          backgroundColor: Colors.cyan[300],
         ),
         body: GoogleMap(
+          onCameraMove: _onCameraMove,
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+            target: _currentPosition,
+            zoom: 16.0,
           ),
         ),
       ),
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    _currentPosition = position.target;
   }
 }
