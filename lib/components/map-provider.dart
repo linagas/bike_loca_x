da39f13a509b7f7d23a88fc1f4dad6b7c5fe30b6
Deivy Hernandez <deivyhernandezperez@gmail.com>
2020-06-28 17:35:14 -0400

    create marker and geolocation

--- lib/components/map-provider.dart
+++ lib/components/map-provider.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:geolocation/geolocation.dart';
 
 class MapProvider extends StatefulWidget {
-  LatLng _current = const LatLng(45.521563, -122.677433);
-
   @override
-  _MapProviderState createState() => _MapProviderState(_current);
-
-  Future<void> _getCurrentLocation() {
-    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
-
-    geolocator
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-        .then((Position position) {
-      return LatLng(position.latitude, position.longitude);
-    }).catchError((e) {
-      print(e);
-    });
-  }
+  _MapProviderState createState() => _MapProviderState();
 }
 
 class _MapProviderState extends State<MapProvider> {
   GoogleMapController mapController;
   LatLng _currentPosition;
+  BitmapDescriptor pinLocationIcon;
+  Set<Marker> _markers = {};
 
-  _MapProviderState(LatLng _currentPosition) {
-    this._currentPosition = _currentPosition;
+  @override
+  void initState() {
+    super.initState();
+    checkGps().then((latLng) => {
+          setState(() {
+            _currentPosition = latLng;
+          }),
+          addMarker(),
+        });
   }
 
   @override
@@ -34,12 +29,13 @@ class _MapProviderState extends State<MapProvider> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Mapa BikeLoca'),
+          title: Text('BikeLoca'),
           backgroundColor: Colors.cyan[300],
         ),
         body: GoogleMap(
           onCameraMove: _onCameraMove,
           onMapCreated: _onMapCreated,
+          markers: _markers,
           initialCameraPosition: CameraPosition(
             target: _currentPosition,
             zoom: 16.0,
@@ -55,5 +51,26 @@ class _MapProviderState extends State<MapProvider> {
 
   void _onCameraMove(CameraPosition position) {
     _currentPosition = position.target;
+  }
+
+  checkGps() async {
+    LocationResult result = await Geolocation.lastKnownLocation();
+    if (result.isSuccessful) {
+      return LatLng(result.location.latitude, result.location.longitude);
+    }
+  }
+
+  void addMarker() async {
+    final pinLocationIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(106, 106)),
+        'assets/images/bike-icon.png');
+    Marker resultMarker = Marker(
+      markerId: MarkerId("<user-id>"),
+      position: _currentPosition,
+      icon: pinLocationIcon,
+    );
+    setState(() {
+      _markers.add(resultMarker);
+    });
   }
 }
