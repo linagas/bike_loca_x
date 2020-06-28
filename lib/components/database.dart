3848697b9a773395b29bd053bd63d70732861520
Daniel Alvarez <elquimeras@gmail.com>
2020-06-28 00:43:46 -0400

    Add database page component

---
+++ lib/components/database.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Database extends StatelessWidget {
+  // const Database({Key key}) : super(key: key);
+  // final databaseReference = Firestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFF2DBD3A));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// Own
 import 'components/welcome.dart';
 
 void main() {
