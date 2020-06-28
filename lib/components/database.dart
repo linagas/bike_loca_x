d5b336d0a4f80e8f56b993d10115595c4e474e06
Daniel Alvarez <elquimeras@gmail.com>
2020-06-28 01:24:56 -0400

    Database dummy content

--- lib/components/database.dart
+++ lib/components/database.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Database extends StatelessWidget {
-  // const Database({Key key}) : super(key: key);
-  // final databaseReference = Firestore.instance;
-
   @override
   Widget build(BuildContext context) {
-    return Container(color: const Color(0xFF2DBD3A));
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Database'),
+              backgroundColor: Colors.black,
+            ),
+            body: Center(
+                child: Container(
+                    color: Colors.white,
+                    child: ListView(
+                      padding: const EdgeInsets.all(8),
+                      children: <Widget>[
+                        Container(
+                          height: 50,
+                          color: Colors.amber[600],
+                          child: const Center(child: Text('Entry A')),
+                        ),
+                        Container(
+                          height: 50,
+                          color: Colors.amber[500],
+                          child: const Center(child: Text('Entry B')),
+                        ),
+                        Container(
+                          height: 50,
+                          color: Colors.amber[100],
+                          child: const Center(child: Text('Entry C')),
+                        ),
+                      ],
+                    )))));
   }
 }
