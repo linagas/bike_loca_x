d133d84433d9fd48928f667c5bacfc6ab52e14fb
Daniel Alvarez <elquimeras@gmail.com>
2020-06-28 20:57:14 -0400

    Set park pin in map from database

--- lib/components/map-provider.dart
+++ lib/components/map-provider.dart
@@ -1,7 +1,10 @@
-import 'package:bike_loca_x/components/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocation/geolocation.dart';
+// Own
+import 'package:bike_loca_x/components/drawer.dart';
+import '../service_locator.dart';
+import '../services/storage_service.dart';
 
 class MapProvider extends StatefulWidget {
   @override
@@ -9,6 +12,7 @@ class MapProvider extends StatefulWidget {
 }
 
 class _MapProviderState extends State<MapProvider> {
+  final StorageService _storageService = locator<StorageService>();
   GoogleMapController mapController;
   LatLng _currentPosition;
   BitmapDescriptor pinLocationIcon;
@@ -23,6 +27,7 @@ class _MapProviderState extends State<MapProvider> {
           }),
           addMarker(),
         });
+    getRecords();
   }
 
   @override
@@ -61,6 +66,33 @@ class _MapProviderState extends State<MapProvider> {
     if (result.isSuccessful) {
       return LatLng(result.location.latitude, result.location.longitude);
     }
+  }
+
+  /*
+   * Get records from _storageService
+   */
+  void getRecords() async {
+    final bikeParkIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(106, 106)),
+        'assets/images/bike-park.jpeg');
+    var latLng;
+    Marker resultMarker;
+
+    var records = await _storageService.getRecords();
+    var id = 1;
+    records.forEach((e) => {
+          print(e),
+          latLng = LatLng(e['lat'].toDouble(), e['lon'].toDouble()),
+          resultMarker = Marker(
+            markerId: MarkerId(id.toString()),
+            position: latLng,
+            icon: bikeParkIcon,
+          ),
+          setState(() {
+            _markers.add(resultMarker);
+          }),
+          id++
+        });
   }
 
   void addMarker() async {
